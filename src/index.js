bdb0a5574590a57225a0900c3c55d397d4a96f7f
pranshuPant07 <pranshupantofficial@gmail.com>
2025-01-29 11:37:03 +0530

    m

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per window
-    message: { success: false, message: 'Too many requests, please try again later.' }});
+    message: { success: false, message: 'Too many requests, please try again later.' }
+});
 
 const PORT = process.env.PORT || 3000;
 const app = express();
